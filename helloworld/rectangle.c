b897febfe3c97328fb69e052a02f6499b23aac93
Donna <donnajun1214@gmail.com>
2022-10-16 19:38:42 +0900

    enum tutorial

--- helloworld/rectangle.c
+++ helloworld/rectangle.c
@@ -10,5 +10,15 @@ int main()
     printf("height: %.2f, width: %.3f, perimete: %f\n",height, width, perimeter);
     printf("area is %f\n", area);
 
+    enum company{
+        GOOGLE, FACEBOOK, XEROX, YAHOO, EBAY, MICROSOFT,
+    };
+    enum company xerox = XEROX;
+    enum company google = GOOGLE;
+    enum company ebay = EBAY;
+
+    printf("The value of xerox is %d\n",xerox);
+    printf("The value of google is %d\n",google);
+    printf("The value of ebay is %d\n",ebay);
     return 0;
 }
